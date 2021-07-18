425cfa1bea8534ca1102081d6fd80d52b6dad7da
RedCarrot0803 <redcarrot0803@gmail.com>
2021-07-19 08:51:29 +0900

    feat: delete package.json

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import people_also_ask
 
-ask_quesions = people_also_ask.get_related_questions("what is a business course?", 14)
+ask_quesions = people_also_ask.get_related_questions("what is webcam?", 14)
 i = 0
 while i < len(ask_quesions):
     print(i + 1)
     print(ask_quesions[i])
     answer_body = people_also_ask.get_answer( ask_quesions[i] )
-    print( answer_body['header'] )
-    print( answer_body['response'] )
+    print( answer_body )
     i += 1
     
     
